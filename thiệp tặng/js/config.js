6ac21f524a7352103273c853506a38d060c211cf
dungdang <dangdunghn1409@gmail.com>
2024-12-22 09:33:19 +0700

    update all

---
+++ thiệp tặng/js/config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'bla';
+const giftUrl = 'https://pnrtscr.com/kqrkc7link';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = 'Tặng iaus';
+const contentCard = 'Chúc wdj thêm xinh đẹp , gặp nhiều may mắn trong cuộc sống và kiếm được ny nhó';
+
+const giftImage = 'download.jpeg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
